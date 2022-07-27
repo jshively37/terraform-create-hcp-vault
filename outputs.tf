9dab4de567fe4dd51bebf7cdd8a3d71248caf976
Jeffrey <jeffrey.shively@hashicorp.com>
2022-07-27 15:38:04 -0400

    Added in conditional logic if public_url is false to not show public_endpoint

---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "hcp_vault_public_url" {
+    value = var.hcp_vault_public_endpoint_bool =="true" ? resource.hcp_vault_cluster.hcp_cluster.vault_public_endpoint_url : null
+}
+
+output "hcp_vault_private_url" {
+    value = resource.hcp_vault_cluster.hcp_cluster.vault_private_endpoint_url
+}
